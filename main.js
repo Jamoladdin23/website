7370088175a3f411f0195daa46401c522ea2f99c
Jamoladdin <djamazar23@gmail.com>
2025-03-12 17:49:04 +0100

    new3

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
-$(function() {
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-  // sizeSections sets the height of each section element to the viewport
-  // height unless the content height is larger than the the viewport
-  // requires the inner content to be wrapped in a .content div
-  var sizeSections = function() {
-    var viewHeight = $(window).height();
-    $("section").each(function(){
-      var naturalHeight = $(this).find(".content").outerHeight();
-      if ( naturalHeight > viewHeight) {
-        $(this).css("height", "auto");
-      } else {
-        $(this).css("height", viewHeight + "px");
-      }
-    });
-  }
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
 
-  // call sizeSections on load
-  sizeSections();
-  // attach smoothScroll jQuery plugin to indicated links
-  $('.navbar a').smoothScroll();
-
-  // call sizeSections whenever the window is resized
-  $(window).resize(function() {
-    sizeSections();
-  });
-
-});
-
+const typed = new Typed(".text", {
+    strings: ["Starter Frontend Developer"],
+    typeSpeed: 90,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
